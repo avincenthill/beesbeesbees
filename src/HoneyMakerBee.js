07fc79d01ab3ce53c37c127875c63ea825b61424
Alexander Vincent-Hill <alexanderjvincenthill@gmail.com>
2018-08-06 10:48:33 -0700

    Pass ForagerBee tests

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,12 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function(color) {
+  Bee.call(this, color);
+  this.age = 10;
+  this.job = "make honey";
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+HoneyMakerBee.prototype.makeHoney = function {
+  this.honeyPot++;
+}
